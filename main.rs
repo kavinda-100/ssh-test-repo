bbea14a0681fe08c6dee0d644e4901774d065595
kavinda-100 <kavindarathnayake100@gmail.com>
2025-07-17 10:57:35 +0530

    add struct in main.rs

--- main.rs
+++ main.rs
@@ -1,4 +1,9 @@
 fn main(){
     println!("Hello, world!");
     // This is a simple Rust program that prints "Hello, world!" to the console.
+
+    struct Person {
+        name: String,
+        age: u32,
+    }
 }
