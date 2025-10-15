a41846419e2bda037b948d51a79e090936c1635c
kavinda-100 <kavindarathnayake100@gmail.com>
2025-10-15 19:38:21 +0530

    Add index.ts with greeting function and console logs

--- index.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, world!");
-console.log("Hello, world!");
-
-function greet(name) {
-  console.log(`Hello, ${name}!`);
-  console.log('done.')
-}
---
+++ index.ts
@@ -0,0 +1,7 @@
+console.log('Hello, world!');
+console.log('Hello, world!');
+
+function greet(name: string) {
+	console.log(`Hello, ${name}!`);
+	console.log('done.');
+}
